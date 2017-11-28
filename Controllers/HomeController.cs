e7e8a6fe27dac28bd879d066614bb7a3334e169e
Olivier Bordes <olivier.bordes@outlook.com>
2017-11-28 16:27:47 +0100

    Renames

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace SpaVue.Controllers
+namespace SpaVuetify.Controllers
 {
     public class HomeController : Controller
     {
